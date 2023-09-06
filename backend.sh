339f93459dfa9d896938f092372a84bfca86b7a9
rajeshfegade <rajeshfegade@gmail.com>
2023-09-06 22:18:36 +0530

    added code change

--- backend.sh
+++ backend.sh
@@ -1,12 +1,10 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 dnf install nodejs -y
-
+cp  backend.service  /etc/systemd/system/backend.service
 useradd expense
 
 mkdir /app
-
-cp  backend.service  /etc/systemd/system/backend.service
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app

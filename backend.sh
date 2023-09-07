c7328ced016d1b82863912d2f8585a4666e94cc2
rajeshfegade <rajeshfegade@gmail.com>
2023-09-07 22:11:25 +0530

    added rm message for rerun code

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 dnf install nodejs -y
 cp  backend.service  /etc/systemd/system/backend.service
 useradd expense
-
+rm -rf /app
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip

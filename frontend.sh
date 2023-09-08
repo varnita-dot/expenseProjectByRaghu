496974447dc914324f1e3d5aaef27bbeeaf5738a
rajeshfegade <rajeshfegade@gmail.com>
2023-09-08 17:38:29 +0530

    Added variable here

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,27 @@
-
+log_message = /tmp/expense.log
 
 echo installing nginx
-dnf install nginx -y >>/tmp/expense.log
+dnf install nginx -y >>log_message
 
-cp  expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
+cp  expense.conf /etc/nginx/default.d/expense.conf >>log_message
 
 echo enable and start nginx
-systemctl enable nginx >>/tmp/expense.log
-systemctl start nginx >>/tmp/expense.log
+systemctl enable nginx >>log_message
+systemctl start nginx >>log_message
 
 echo removed old content
-rm -rf /usr/share/nginx/html/*   >>/tmp/expense.log
+rm -rf /usr/share/nginx/html/*   >>log_message
 
 echo download frontend.zip
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >>log_message
 
 echo change the directory
-cd /usr/share/nginx/html  >>/tmp/expense.log
+cd /usr/share/nginx/html  >>log_message
 
 echo unzip frontend.zip
-unzip /tmp/frontend.zip  >>/tmp/expense.log
+unzip /tmp/frontend.zip  >>log_message
 
 echo restart nginx
-systemctl restart nginx  >>/tmp/expense.log
+systemctl restart nginx  >>log_message
 
 echo end of frontend

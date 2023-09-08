a3462ce837fbbc4a7810975798b8dff0b245b77e
rajeshfegade <rajeshfegade@gmail.com>
2023-09-08 21:33:24 +0530

    added & to capture all out ans error message

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ log_message=/tmp/expense.log
 download_and_extract(){
   echo inside function
   echo download $component.zip
-  curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip   &&>>$log_message
+  curl  -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip   &&>>$log_message
   echo $?
 
   echo unzip $component.zip

83cf9859beb6c11794c169621c456f5239a98c91
rajeshfegade <rajeshfegade@gmail.com>
2023-09-08 18:30:28 +0530

    added variable name in all placed

--- backend.sh
+++ backend.sh
@@ -1,23 +1,24 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+log_message=/tmp/expense.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  >>$log_message
 
-dnf install nodejs -y
-cp  backend.service  /etc/systemd/system/backend.service
-useradd expense
-rm -rf /app
-mkdir /app
+dnf install nodejs -y  >>$log_message
+cp  backend.service  /etc/systemd/system/backend.service  >>$log_message
+useradd expense  >>$log_message
+rm -rf /app  >>$log_message
+mkdir /app  >>$log_message
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip  >>$log_message
+cd /app  >>$log_message
+unzip /tmp/backend.zip  >>$log_message
 
-cd /app
-npm install
+cd /app  >>$log_message
+npm install  >>$log_message
 
-systemctl daemon-reload
+systemctl daemon-reload  >>$log_message
 
-systemctl enable backend
-systemctl start backend
+systemctl enable backend  >>$log_message
+systemctl start backend  >>$log_message
 
-dnf install mysql -y
+dnf install mysql -y  >>$log_message
 
-mysql -h sql.varraj20316.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h sql.varraj20316.online -uroot -pExpenseApp@1 < /app/schema/backend. >>$log_message
--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,14 @@
-dnf module disable mysql -y
+log_message=/tmp/expense.log
+dnf module disable mysql -y >>$log_message
 
-cp mysql.repo  /etc/yum.repos.d/mysql.repo
+cp mysql.repo  /etc/yum.repos.d/mysql.repo >>$log_message
 
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y >>$log_message
 
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld >>$log_message
+systemctl start mysqld >>$log_message
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 >>$log_message
 
-mysql -uroot -pExpenseApp@1
+mysql -uroot -pExpenseApp@1 >>$log_message
 

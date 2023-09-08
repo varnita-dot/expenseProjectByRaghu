77b7bf60661c5c40c016703ae332cd06942ff87d
rajeshfegade <rajeshfegade@gmail.com>
2023-09-08 15:45:55 +0530

    added rm message for rerun code

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,23 @@
+echo installing nginx
 dnf install nginx -y
+
 cp  expense.conf /etc/nginx/default.d/expense.conf
 
+echo enable and start nginx
 systemctl enable nginx
 systemctl start nginx
 
+echo removed old content
 rm -rf /usr/share/nginx/html/*
 
+echo download frontend.zip
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo change the directory
 cd /usr/share/nginx/html
+
+echo unzip frontend.zip
 unzip /tmp/frontend.zip
 
-
-
+echo restart nginx
 systemctl restart nginx

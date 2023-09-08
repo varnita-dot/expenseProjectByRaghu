d2579c218f78e60253eb370f29343bca765123b8
rajeshfegade <rajeshfegade@gmail.com>
2023-09-08 15:58:18 +0530

    added print code for all statements

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,27 @@
-echo installing nginx
-dnf install nginx -y
 
-cp  expense.conf /etc/nginx/default.d/expense.conf
+
+echo installing nginx
+dnf install nginx -y >>/tmp/expense.log
+
+cp  expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
 
 echo enable and start nginx
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx >>/tmp/expense.log
+systemctl start nginx >>/tmp/expense.log
 
 echo removed old content
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*   >>/tmp/expense.log
 
 echo download frontend.zip
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip   >>/tmp/expense.log
 
 echo change the directory
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html  >>/tmp/expense.log
 
 echo unzip frontend.zip
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  >>/tmp/expense.log
 
 echo restart nginx
-systemctl restart nginx
+systemctl restart nginx  >>/tmp/expense.log
 
 echo end of frontend

3c7f53602bb70d008b48558f0f01ad7e18441c33
rajeshfegade <rajeshfegade@gmail.com>
2023-09-10 19:00:10 +0530

    added & to capture all out ans error message

--- common.sh
+++ common.sh
@@ -4,11 +4,11 @@ download_and_extract(){
   echo inside function
   echo download $component.zip
   curl  -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip   &&>>$log_message
-  echo $?
+  extract_status
 
   echo unzip $component.zip
   unzip /tmp/$component.zip  &>>$log_message
-  echo $?
+  extract_status
   echo end of function
 
 }

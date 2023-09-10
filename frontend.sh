fd040e248bf003be58308d76c97183d36b13e0b3
rajeshfegade <rajeshfegade@gmail.com>
2023-09-10 18:54:12 +0530

    added & to capture all out ans error message

--- frontend.sh
+++ frontend.sh
@@ -11,28 +11,58 @@ if [ $? -eq 0 ]; then
 exit 1
 fi
 cp  expense.conf /etc/nginx/default.d/expense.conf &>>$log_message
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+exit 1
+fi
 
 echo enable and start nginx
 systemctl enable nginx &>>$log_message
 systemctl start nginx &>>$log_message
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+exit 1
+fi
 
 echo removed old content
 rm -rf /usr/share/nginx/html/*   &>>$log_message
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+exit 1
+fi
 
 download_and_extract
 
 echo change the directory
 cd /usr/share/nginx/html  &>>$log_message
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+exit 1
+fi
 
 
 
 echo restart nginx
 systemctl restart nginx  &>>$log_message
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+exit 1
+fi
 
 echo end of frontend
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+exit 1
+fi
